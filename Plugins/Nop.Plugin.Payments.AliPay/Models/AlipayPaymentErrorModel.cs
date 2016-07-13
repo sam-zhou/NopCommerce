403aabc7e02c7bdc2e20f94d59d1d439fd040a9e
Sam Zhou <samzhou.it@gmail.com>
2016-07-13 23:27:29 +0800

    ddddddd
    ds

---
+++ Plugins/Nop.Plugin.Payments.AliPay/Models/AlipayPaymentErrorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Nop.Plugin.Payments.AliPay.Models
+{
+    public class AlipayPaymentErrorModel
+    {
+        public string Message { get; set; }
+
+        public string Title { get; set; }
+
+        public bool HasError
+        {
+            get { return !string.IsNullOrWhiteSpace(Message); }
+        }
+    }
+}
---
+++ Plugins/Nop.Plugin.Payments.AliPay/Models/AlipayPaymentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Payments.AliPay.Models
+{
+    public class AlipayPaymentModel
+    {
+        public string Url { get; set; }
+
+        public string OrderId { get; set; }
+
+        public string Total { get; set; }
+    }
+}
